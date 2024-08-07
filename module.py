9c6623aa17b022302d84c6f81ed3562df6cf20c4
Naresh Dhungana <hinareshg@gmail.com>
2024-08-07 09:49:07 +0900

    ht

--- module.py
+++ module.py
@@ -2,6 +2,8 @@
 import os
 import hashlib
 import shutil
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def calculate_hash_helper(file_path, hash_algo=hashlib.md5, chunk_size=4096):
     hash_obj = hash_algo()
@@ -50,3 +52,32 @@ def copy_pdfs(src_folder='C:/Users/nares/Zotero/storage', dest_folder='C:/Users/
                 dest_file_path = os.path.join(dest_folder, file)
                 shutil.copy2(src_file_path, dest_file_path)
                 print(f"Copied {src_file_path} to {dest_file_path}")
+
+def plot_csv(file_path="C:/Users/nares/source/repos/thesis/data/20230920_134223_contrast - for zoom.csv", x_column="Number of frame", y_column="sd50"):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file_path)
+    df[x_column] = (df[x_column].astype(float))/100
+    df[y_column] = (df[y_column].astype(float))*7000
+    # Check if the specified columns exist in the DataFrame
+    if x_column not in df.columns or y_column not in df.columns:
+        raise ValueError(f"Columns '{x_column}' or '{y_column}' not found in the CSV file.")
+    
+    # Skip the first 20 frames
+    df = df.iloc[64:-40]
+    # Plot the data
+    plt.figure(figsize=(10, 6))
+    
+    plt.plot(df[x_column], df[y_column],)# marker='o')
+    hori = 35.0
+    plt.axhline(y=hori, color='r', linestyle='--', label=hori)  # Add this line to plot the horizontal line at y=0.5
+    plt.axhline(y=hori/2.5, color='g', linestyle='--', label=hori/2.5)  # Add this line to plot the horizontal line at y=0.5
+    # Add labels and title
+    plt.xlabel("Time (s)")
+    plt.ylabel("BD")
+    plt.title('BD for 50 frames')
+    plt.legend()
+    # Show grid
+    plt.grid(False)
+    
+    # Show the plot
+    plt.show()
