65301a58418ab1e90bcbe47fbddcda5c1a5eec66
Naresh Dhungana <hinareshg@gmail.com>
2025-06-08 10:14:06 +0900

    g

--- oopp.py
+++ oopp.py
@@ -12,7 +12,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Apply edge detection, for example using Canny
 edges = cv2.Canny(gray, 50, 150)
 
-# Find contours in the edged image
+# Find conto urs in the edged image
 contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # Draw contours on the original image
